113d5fcfba38b0c36d096006425f1f87a0368d06
Acer <atabekovaayona@gmail.com>
2022-12-20 00:32:22 +0300

    исправила и сделала дз

--- app/src/main/java/com/example/homwork1/MainActivity.java
+++ app/src/main/java/com/example/homwork1/MainActivity.java
@@ -1,29 +1,59 @@
 package com.example.homwork1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    Button button;
+    BottomNavigationView bottomNavigationView;
+    FragmentContainerView fragmentContainerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_naw);
+        fragmentContainerView = findViewById(R.id.fragment_container);
 
-        editText= findViewById(R.id.edit_text);
-        button= findViewById(R.id.send);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment())
+                                .commit();
+                        break;
+                    case R.id.menu_gallery:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new GalleryFragment())
+                                .commit();
+                        break;
+                    case R.id.menu_like:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LikeFragment())
+                                .commit();
+                        break;
+                    case R.id.menu_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment())
+                                .commit();
+                        break;
+                    case R.id.menu_search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment())
+                                .commit();
+                        break;
+                }
+                return true;
+            }
+        });
 
-        button.setOnClickListener(view -> ){
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            startActivity(intent);
-        };
+
     }
 }
